3d404ab98ab7793a6d720e2c70215a29949ef546
BlayeeR <>
2021-01-21 00:05:44 +0100

    setting api url depending on environment, css styling

--- frontend/src/app/document.service.ts
+++ frontend/src/app/document.service.ts
@@ -5,6 +5,7 @@ import { PdfFile } from "@models/PdfFile";
 import { Date as PdfDate } from "@models/Date";
 import { Amount, Vat } from "@models/Amount";
 import { Info } from "@models/Info";
+import { environment } from "../environments/environment";
 
 @Injectable({
     providedIn: "root",
@@ -19,79 +20,87 @@ export class DocumentService {
         fileId: number,
         searchRadius: number
     ): Observable<PdfFile> {
-        const endpoint =
-            "http://localhost/api/files/finddates?id=" +
-            fileId +
-            "&searchradius=" +
-            searchRadius;
-        return this.httpClient.get<PdfFile>(endpoint);
+        return this.httpClient.get<PdfFile>(
+            environment.apiUrl +
+                "/files/finddates?id=" +
+                fileId +
+                "&searchradius=" +
+                searchRadius
+        );
     }
 
     public findAmounts(
         fileId: number,
         searchRadius: number
     ): Observable<PdfFile> {
-        const endpoint =
-            "http://localhost/api/files/findamounts?id=" +
-            fileId +
-            "&searchradius=" +
-            searchRadius;
-        return this.httpClient.get<PdfFile>(endpoint);
+        return this.httpClient.get<PdfFile>(
+            environment.apiUrl +
+                "/files/findamounts?id=" +
+                fileId +
+                "&searchradius=" +
+                searchRadius
+        );
     }
 
     public findInfo(fileId: number, searchRadius: number): Observable<PdfFile> {
-        const endpoint =
-            "http://localhost/api/files/findinfo?id=" +
-            fileId +
-            "&searchradius=" +
-            searchRadius;
-        return this.httpClient.get<PdfFile>(endpoint);
+        return this.httpClient.get<PdfFile>(
+            environment.apiUrl +
+                "/files/findinfo?id=" +
+                fileId +
+                "&searchradius=" +
+                searchRadius
+        );
     }
 
     public saveDocument(
         fileId: number,
         conditions: DocumentConditions
     ): Observable<PdfFile> {
-        const endpoint = "http://localhost/api/files/savedocument?id=" + fileId;
-        return this.httpClient.post<PdfFile>(endpoint, conditions);
+        return this.httpClient.post<PdfFile>(
+            environment.apiUrl + "/files/savedocument?id=" + fileId,
+            conditions
+        );
     }
 
     public async getDocuments() {
-        const endpoint = "http://localhost/api/files/getdocuments";
-        this.documents = await this.httpClient.get<PdfFile[]>(endpoint).toPromise();
+        this.documents = await this.httpClient
+            .get<PdfFile[]>(environment.apiUrl + "/files/getdocuments")
+            .toPromise();
     }
 
     public deleteDocument(id: number) {
-      const endpoint = "http://localhost/api/files/deletedocument?id=" + id;
-      return this.httpClient.delete(endpoint);
+        return this.httpClient.delete(
+            environment.apiUrl + "/files/deletedocument?id=" + id
+        );
     }
 
     public async getDocument(id: number): Promise<PdfFile> {
-      const endpoint = "http://localhost/api/files/getdocument?id=" + id;
-      return await this.httpClient.get<PdfFile>(endpoint).toPromise();
+        return await this.httpClient
+            .get<PdfFile>(environment.apiUrl + "/files/getdocument?id=" + id)
+            .toPromise();
     }
 
     public mapVat(vat: Vat) {
-      switch(vat) {
-        case Vat.vat_0: {
-          return "0%";
+        switch (vat) {
+            case Vat.vat_0: {
+                return "0%";
+            }
+            case Vat.vat_5: {
+                return "5%";
+            }
+            case Vat.vat_8: {
+                return "8%";
+            }
+            case Vat.vat_23: {
+                return "23%";
+            }
+            case Vat.vat_zw: {
+                return "zw";
+            }
+            case Vat.vat_np: {
+                return "np";
+            }
         }
-        case Vat.vat_5: {
-          return "5%";
-        }
-        case Vat.vat_8: {
-          return "8%";
-        }
-        case Vat.vat_23: {
-          return "23%";
-        }
-        case Vat.vat_zw: {
-          return "zw";
-        }
-        case Vat.vat_np: {
-          return "np";
-        }
-      }
     }
 }
 
--- frontend/src/app/file-upload.service.ts
+++ frontend/src/app/file-upload.service.ts
@@ -1,22 +1,26 @@
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { catchError, map } from 'rxjs/operators';
-import { MatSnackBar } from '@angular/material/snack-bar'
-import { PdfFile } from '@models/PdfFile';
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { catchError, map } from "rxjs/operators";
+import { MatSnackBar } from "@angular/material/snack-bar";
+import { PdfFile } from "@models/PdfFile";
+import { environment } from "environments/environment";
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: "root",
 })
 export class FileUploadService {
+    constructor(
+        private httpClient: HttpClient,
+        private snackBar: MatSnackBar
+    ) {}
 
-  constructor(private httpClient: HttpClient, private snackBar: MatSnackBar) { }
-
-  public postFile(fileToUpload: File): Observable<PdfFile> {
-    const endpoint = 'http://localhost/api/files/upload';
-    const formData: FormData = new FormData();
-    formData.append('fileKey', fileToUpload, fileToUpload.name);
-    return this.httpClient
-      .post<PdfFile>(endpoint, formData);
-  }
+    public postFile(fileToUpload: File): Observable<PdfFile> {
+        const formData: FormData = new FormData();
+        formData.append("fileKey", fileToUpload, fileToUpload.name);
+        return this.httpClient.post<PdfFile>(
+            environment.apiUrl + "/files/upload",
+            formData
+        );
+    }
 }
