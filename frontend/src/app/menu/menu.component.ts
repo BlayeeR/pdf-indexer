9b6653423ec75fcf1667d466daf705e4f7e0794f
BlayeeR <>
2021-01-23 01:02:17 +0100

    menu fix

--- frontend/src/app/menu/menu.component.ts
+++ frontend/src/app/menu/menu.component.ts
@@ -1,34 +1,35 @@
-import { Component, OnInit } from '@angular/core';
-import { NavigationEnd, Router } from '@angular/router';
-import { Subscription } from 'rxjs';
-import { MainService } from '../main.service';
+import { Component, OnInit } from "@angular/core";
+import { NavigationEnd, Router } from "@angular/router";
+import { Subscription } from "rxjs";
+import { MainService } from "../main.service";
 
 @Component({
-  selector: 'app-menu',
-  templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.scss']
+    selector: "app-menu",
+    templateUrl: "./menu.component.html",
+    styleUrls: ["./menu.component.scss"],
 })
 export class MenuComponent implements OnInit {
+    private routerEventSubscription: Subscription;
+    public activeRoute: string;
 
-  private routerEventSubscription: Subscription;
-  public activeRoute: string;
+    constructor(private router: Router, public mainService: MainService) {}
 
-  constructor(private router: Router, public mainService: MainService) {
-  }
+    ngOnDestroy() {
+        this.routerEventSubscription.unsubscribe();
+    }
 
-  ngOnDestroy() {
-    this.routerEventSubscription.unsubscribe();
-  }
-
-  ngOnInit(): void {
-    this.routerEventSubscription = this.router.events.subscribe((event: NavigationEnd) => {
-      if(event.url) {
-        const option = this.mainService.menuOptions.find(x=>x.Route === event.url);
-        if(option) {
-          this.activeRoute = option.Name;
-        }
-      }
-    });
-  }
-
+    ngOnInit(): void {
+        this.routerEventSubscription = this.router.events.subscribe(
+            (event: NavigationEnd) => {
+                if (event.url) {
+                    const option = this.mainService.menuOptions.find(
+                        (x) => x.Route === event.urlAfterRedirects
+                    );
+                    if (option) {
+                        this.activeRoute = option.Name;
+                    }
+                }
+            }
+        );
+    }
 }
